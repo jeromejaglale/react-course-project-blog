b70fa60032c596f820178fa6e1b0a2d17b6e47fe
Jerome Jaglale <jerome_jaglale@sfu.ca>
2023-12-11 23:35:13 -0800

    Stretch goal 1: RSS Feed (part 2)

--- src/app/rss.xml/route.js
+++ src/app/rss.xml/route.js
@@ -1,16 +1,34 @@
+import { getBlogPostList } from '@/helpers/file-helpers';
+
 export const dynamic = 'force-dynamic' // defaults to auto
  
-export async function GET() {
-  return new Response(`<?xml version="1.0" encoding="UTF-8" ?>
-<rss version="2.0">
+export async function GET() { 
+/* create rss feed */
+var RSS = require('rss');
+var feed = new RSS({
+    title: 'Bits & Bytes',
+    custom_namespaces: {
+      'itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd'
+    },
+});
+
+const blogPostList = await getBlogPostList();
+
+/* loop over data and add to feed */
+blogPostList.map(p => {
+	feed.item({
+	    title: p.title,
+	    description: p.abstract,
+	    url: `localhost/${p.slug}`,
+	    date: new Date(p.publishedOn), // any format that js Date can parse.
+	});
+
+});
  
-<channel>
-  <title>Next.js Documentation</title>
-  <link>https://nextjs.org/docs</link>
-  <description>The React Framework for the Web</description>
-</channel>
- 
-</rss>`, {
+// cache the xml to send to clients
+var xml = feed.xml();
+
+  return new Response(xml, {
     headers: { 'Content-Type': 'application/xml' },
   })
 }
